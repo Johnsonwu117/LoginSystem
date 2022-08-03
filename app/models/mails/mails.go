e7a5baf27b8397d2438178a885464d341503d4f1
johnson <jo890117jo890117@gmail.com>
2022-08-03 16:51:46 +0800

    改版

--- app/models/mails/mails.go
+++ app/models/mails/mails.go
@@ -23,6 +23,7 @@ type User struct {
 }
 
 var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 var (
 	host     = "smtp.gmail.com:587"
 	username = "jo890117jo890117@gmail.com"
@@ -44,7 +45,7 @@ func CreateFirst(user User) User {
 	db.DB.Create(&user).Table("users").Save(map[string]interface{}{"Invitecode": incode})
 	auth := smtp.PlainAuth(host, username, password, "smtp.gmail.com")
 	to := []string{user.Email}
-	str := fmt.Sprintf("From:jo890117jo890117@gmail.com\r\nTo:%v\r\nSubject:信箱驗證碼\r\n\r\n親愛的會員你好:\r\n\r\n感謝你註冊會員，以下為你的邀請碼:\r\n\r\n這是你的邀請碼:%08v\r\n\r\n請多多邀請別人。\r\n", user.Email, incode)
+	str := fmt.Sprintf(Mail1(), user.Email, incode)
 	msg := []byte(str)
 	smtp.SendMail(
 		host,
---
+++ app/models/mails/word.go
@@ -0,0 +1,7 @@
+package mails
+
+func Mail1() string {
+
+	a := ("From:jo890117jo890117@gmail.com\r\nTo:%v\r\nSubject:信箱驗證碼\r\n\r\n親愛的會員你好:\r\n\r\n感謝你註冊會員，以下為你的邀請碼:\r\n\r\n這是你的邀請碼:%08v\r\n\r\n請多多邀請別人。\r\n")
+	return a
+}
